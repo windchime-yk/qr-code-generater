41a6eec9271004a2e0ac3dfa0c4429664877005d
windchime-yk <windchimeyk@gmail.com>
2019-04-03 01:17:52 +0900

    modify: reset repo

--- src/js/entrypoint.js
+++ src/js/entrypoint.js
@@ -1,21 +1,7 @@
-import qrcode from 'qrcode';
-
-const qrdraw = document.getElementById('qrdraw');
-const geneBtn = document.getElementById('generateBtn');
-const saveBtn = document.getElementById('saveBtn');
-
-geneBtn.addEventListener('click', () => {
-  const geneVal = document.getElementById('inputUrl').value;
-  if(geneVal === '') {
-    alert('URLを入力してください');
-  } else if(geneVal.match(/\.js$|\.zip$/g)) {
-    alert('無効なURLです');
-  } else {
-    qrcode.toCanvas(qrdraw, geneVal, error => {
-      if(error) console.error(error);
-      else if(!error) alert('作成に成功しました');
-    });
-    const imgData = qrdraw.toDataURL('image/png');
-    saveBtn.href = imgData;
+const vue = new vue({
+  el: '#app',
+  data: {
+    url: '',
+    size: 500
   }
 });
